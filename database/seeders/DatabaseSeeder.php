1d7f702d834a83518cab7b779455db309c0d67da
Soumyajit Ball <soumyajit.ball@ssil.com>
2023-03-29 19:36:22 +0530

    database seeder uncommented

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(CountriesTableDataSeeder::class);
-        // $this->call(StatesTableDataSeeder::class);
+        $this->call(CountriesTableDataSeeder::class);
+        $this->call(StatesTableDataSeeder::class);
         $this->call(AdminSeeder::class);
 
     }
